6a2448a54d541db70aca45a3971e3b4517846c77
Chu Phong <man.nguyen.cit@gmail.com>
2023-10-06 01:11:23 +0700

    update productDetail

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import { LOCATIONS } from 'constants/index';
 import  Layout  from 'components/layout';
 import Main from 'components/main';
 import AboutPage from 'pages/about';
+import ProductDetail from 'components/productDetail';
 export const routers = [
     //   { path: LOCATIONS.LOGIN, name: "Login Page", element: <Login /> },
       {
@@ -11,6 +12,7 @@ export const routers = [
         children: [
           { isRoot: true, name: "Home", element: <Main/> },
           { path: LOCATIONS.ABOUT_PAGE, name: "About", element: <AboutPage /> },
+          { path: LOCATIONS.PRODUCT_DETAILS, element: <ProductDetail /> },
         //   { path: LOCATIONS.FORM, name: "Form Register", element: <FormPage /> },
         //   { path: LOCATIONS.TAB, name: "Tab", element: <TabPage /> },
         //   { path: LOCATIONS.SLIDE, name: "Slider", element: <SliderPage /> },
