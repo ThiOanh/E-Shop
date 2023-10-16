241348e9a5d1aca83cba7f3b787369afb456df9e
thailehungvn <thailehungvn@gmail.com>
2023-10-17 02:51:37 +0700

    update

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import gamepad from "assets/product/gamePad.png";
+import laptop from "assets/product/laptop.png";
 import { Link } from "react-router-dom";
 import { LOCATIONS } from "constants/index";
 
-
 function cart(props) {
   return (
-    
     <div className="container">
       {/* header link  */}
       <div className="d-flex my-5 text-black-50 form-text">
@@ -18,29 +18,27 @@ function cart(props) {
       </div>
 
       <table class="table">
-          <thead>
-            <tr>
-              <th> Product</th>
-              <th> Price</th>
-              <th> Quantity</th>
-              <th> Subtotal</th>
-              <th> Remove</th>
-            </tr>
-          </thead>
-          <tbody>         
+        <thead>
+          <tr>
+            <th> Product</th>
+            <th> Price</th>
+            <th> Quantity</th>
+            <th> Subtotal</th>
+            <th> Remove</th>
+          </tr>
+        </thead>
+        <tbody>
           <tr>
             <td>
-            <img
-            src={require("assets/images/cart/monitor.png")}
-            className="Ltd Monitor"
-            alt="..."
-          />
-          
-              <td>Ltd Monitor</td>
+              <div className="d-flex align-items-center">
+                <img src={gamepad} alt="gamepad" width={100} height="auto" />
+                <div className="mx-2">LCD Monitor</div>
+              </div>
             </td>
 
             <td>650$</td>
             <td>
+              
               <button class="decrease()">-</button>
               <input type="number" max={100} min={1} defaultValue={1} />
               <button class="increase()">+</button>
@@ -48,73 +46,68 @@ function cart(props) {
             </td>
             <td>19$</td>
             <td>
-              <button class="btn btn-danger">
-                Delete
-              </button>
+              <button class="btn btn-danger">Delete</button>
             </td>
           </tr>
           <tr>
             <td>
-            <img
-            src={require("assets/images/cart/gamePad.png")}
-            className="h1 Gamepad"
-            alt="..."
-          />
-              <td>H1 Gamepad</td>
+              <div className="d-flex align-items-center">
+                <img src={laptop} alt="laptop" width={100} height="auto" />
+                <div className="mx-2">H1 GamePad</div>
+              </div>
             </td>
             <td>550$</td>
             <td>
-            <button class="decrease()">-</button>
+              <button class="decrease()">-</button>
               <input type="number" max={100} min={1} defaultValue={1} />
               <button class="decrease()">+</button>
             </td>
             <td>19$</td>
             <td>
-            <button class="btn btn-danger">
-                Delete
-              </button>
+              <button class="btn btn-danger">Delete</button>
             </td>
           </tr>
-          </tbody>
-        </table>
-        <br />
-        <div className="d-flex justify-content-between">
-          <div>
-            <button>Return To Shop</button>
-            
-            </div>
-            
-          <div> <button>Update Cart</button></div>
+        </tbody>
+      </table>
+      <br />
+      <div className="d-flex justify-content-between">
+        <div>
+          <button>Return To Shop</button>
         </div>
-        <br />
-        <div className="d-flex justify-content-between">
-          <div>
-            <input type="text" placeholder="coupon Code" />
-            <button>Apply Coupon</button>
+
+        <div>
+          {" "}
+          <button>Update Cart</button>
+        </div>
+      </div>
+      <br />
+      <div className="d-flex justify-content-between">
+        <div>
+          <input type="text" placeholder="coupon Code" />
+          <button class="btn btn-danger">Apply Coupon</button>
+        </div>
+        <div >
+          <h3>Cart Total</h3>
+          <div className="d-flex justify-content-between">
+            <p>Subtotal:</p>
+            <p>1750$</p>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>Shiping:</p>
+            <p>free</p>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>Total:</p>
+            <p>1750$</p>
           </div>
           <div>
-            <h3>Cart Total</h3>
-            <div className="d-flex justify-content-between">
-              <p>Subtotal:</p>
-              <p>1750$</p>
-            </div>
-            <div className="d-flex justify-content-between">
-              <p>Shiping:</p>
-              <p>free</p>
-            </div>
-            <div className="d-flex justify-content-between">
-              <p>Total:</p>
-              <p>1750$</p>
-            </div>
-            <div>
-              <Link to={LOCATIONS.CHECKOUT}>
-                <button className="btn btn-danger">Process Checkout</button>
-              </Link>
-            </div>
+            <Link to={LOCATIONS.CHECKOUT}>
+              <button className="btn btn-danger">Process Checkout</button>
+            </Link>
           </div>
         </div>
       </div>
-    
+    </div>
   );
 }
 
