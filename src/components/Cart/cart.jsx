c6969953f764bf0e8347f6f11b4b771c750d597d
thailehungvn <thailehungvn@gmail.com>
2023-10-18 01:26:18 +0700

    update

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -3,17 +3,16 @@ import gamepad from "assets/product/gamePad.png";
 import laptop from "assets/product/laptop.png";
 import { Link } from "react-router-dom";
 import { LOCATIONS } from "constants/index";
-
-
-
-
+import "./cart.module.scss";
 
 function cart(props) {
   return (
     <div className="container">
       {/* header link  */}
-      <div className="d-flex my-5 text-black-50 form-text">
-        <li><Link to={LOCATIONS.HOME_PAGE}>Home</Link></li>
+      <div className="d-flex my-5 text-black-50  ">
+        <li>
+          <Link to={LOCATIONS.HOME_PAGE}>Home</Link>
+        </li>
         <span className="mx-2">/</span>
         {/* active text-body */}
         <a href="/Cart" className="text-body">
@@ -34,41 +33,44 @@ function cart(props) {
         <tbody>
           <tr>
             <td>
-              <div className="d-flex align-items-center">
-                <img src={gamepad} alt="gamepad" width={100} height="auto" />
+              <div className="d-flex align-items-center my-5">
+                <img src={gamepad} alt="gamepad" width={54} height={54}  />
                 <div className="mx-2">LCD Monitor</div>
               </div>
             </td>
 
-            <td>650$</td>
             <td>
-              
-              <button class="decrease()">-</button>
+              <div className="my-5">650$</div>
+            </td>
+
+            <td>
+              <button class="decrease() my-5">-</button>
               <input type="number" max={100} min={1} defaultValue={1} />
               <button class="increase()">+</button>
               <p id="demo"></p>
             </td>
-            <td>19$</td>
+            <td><div className="my-5">650$</div></td>
             <td>
-              <button class="btn btn-danger">Delete</button>
+              <button class="btn btn-danger my-5">Delete</button>
             </td>
+            
           </tr>
           <tr>
             <td>
-              <div className="d-flex align-items-center">
-                <img src={laptop} alt="laptop" width={100} height="auto" />
+              <div className="d-flex align-items-center my-5">
+                <img src={laptop} alt="laptop" width={50} height={50} />
                 <div className="mx-2">H1 GamePad</div>
               </div>
             </td>
-            <td>550$</td>
+            <td><div className="my-5">650$</div></td>
             <td>
-              <button class="decrease()">-</button>
+              <button class="decrease() my-5 ">-</button>
               <input type="number" max={100} min={1} defaultValue={1} />
               <button class="decrease()">+</button>
             </td>
-            <td>19$</td>
+            <td><div className="my-5">650$</div></td>
             <td>
-              <button class="btn btn-danger">Delete</button>
+              <button class="btn btn-danger my-5">Delete</button>
             </td>
           </tr>
         </tbody>
@@ -76,12 +78,13 @@ function cart(props) {
       <br />
       <div className="d-flex justify-content-between">
         <div>
-          <button>Return To Shop</button>
+        <button type="button" class="btn btn-outline-danger style">Return To Shop</button>
+         
         </div>
 
         <div>
-          {" "}
-          <button>Update Cart</button>
+        
+        <button type="button" class="btn btn-outline-danger style">Update Cart</button>
         </div>
       </div>
       <br />
@@ -90,33 +93,29 @@ function cart(props) {
           <input type="text" placeholder="coupon Code" />
           <button class="btn btn-danger">Apply Coupon</button>
         </div>
-       
-       
+
         <span class="border border-dark">
-        <div >
-          <text className="text">Cart Total</text>
-          <div className="d-flex justify-content-between">
-            <p>Subtotal:</p>
-            <p>1750$</p>
-          </div>
-          <div className="d-flex justify-content-between">
-            <p>Shiping:</p>
-            <p>free</p>
-          </div>
-          <div className="d-flex justify-content-between">
-            <p>Total:</p>
-            <p>1750$</p>
-          </div>
           <div>
-            <Link to={LOCATIONS.CHECKOUT}>
-              <button className="btn btn-danger">Process Checkout</button>
-            </Link>
+            <text className="text">Cart Total</text>
+            <div className="d-flex justify-content-between">
+              <p>Subtotal:</p>
+              <p>1750$</p>
+            </div>
+            <div className="d-flex justify-content-between">
+              <p>Shiping:</p>
+              <p>free</p>
+            </div>
+            <div className="d-flex justify-content-between">
+              <p>Total:</p>
+              <p>1750$</p>
+            </div>
+            <div>
+              <Link to={LOCATIONS.CHECKOUT}>
+                <button className="btn btn-danger">Process Checkout</button>
+              </Link>
+            </div>
           </div>
-          </div>
-          </span>
-       
-
-
+        </span>
       </div>
     </div>
   );
