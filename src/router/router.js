7f245c9544b28334b3129cfd9ace5545a9d740e5
ndbhung1810 <Nguyendbhung1810@gmail.com>
2023-10-19 19:31:24 +0700

    fix slide

--- src/router/router.js
+++ src/router/router.js
@@ -1,31 +1,27 @@
-import { LOCATIONS } from 'constants/index';
-import Layout from 'components/layout';
-import Main from 'components/main';
-import AboutPage from 'pages/about';
-import ProductDetail from 'components/productDetail';
-import Cart from 'components/Cart/cart';
-import Checkout from 'components/checkout/Checkout';
+import Cart from "components/Cart/cart";
+import Checkout from "components/checkout/checkout";
+import Layout from "components/layout";
+import Main from "components/main";
+import ProductDetail from "components/productDetail";
+import { LOCATIONS } from "constants/index";
+import AboutPage from "pages/about";
 export const routers = [
-    //   { path: LOCATIONS.LOGIN, name: "Login Page", element: <Login /> },
-    {
-        path: LOCATIONS.HOME_PAGE,
-        name: "Layout",
-        element: < Layout / > ,
-        children: [
-            { isRoot: true, name: "Home", element: < Main / > },
-            { path: LOCATIONS.ABOUT_PAGE, name: "About", element: < AboutPage / > },
-            { path: LOCATIONS.PRODUCT_DETAILS, element: < ProductDetail / > },
-            { path: LOCATIONS.CART, element: < Cart / > },
-            { path: LOCATIONS.CHECKOUT, element: < Checkout / > },
+  //   { path: LOCATIONS.LOGIN, name: "Login Page", element: <Login /> },
+  {
+    path: LOCATIONS.HOME_PAGE,
+    name: "Layout",
+    element: <Layout />,
+    children: [
+      { isRoot: true, name: "Home", element: <Main /> },
+      { path: LOCATIONS.ABOUT_PAGE, name: "About", element: <AboutPage /> },
+      { path: LOCATIONS.PRODUCT_DETAILS, element: <ProductDetail /> },
+      { path: LOCATIONS.CART, element: <Cart /> },
+      { path: LOCATIONS.CHECKOUT, element: <Checkout /> },
 
-
-
-
-            //   { path: LOCATIONS.FORM, name: "Form Register", element: <FormPage /> },
-            //   { path: LOCATIONS.TAB, name: "Tab", element: <TabPage /> },
-            //   { path: LOCATIONS.SLIDE, name: "Slider", element: <SliderPage /> },
-            //   { path: LOCATIONS.MY_PROFILE, name: "My Profile", element: <Profile /> },
-
-        ]
-    },
-]
+      //   { path: LOCATIONS.FORM, name: "Form Register", element: <FormPage /> },
+      //   { path: LOCATIONS.TAB, name: "Tab", element: <TabPage /> },
+      //   { path: LOCATIONS.SLIDE, name: "Slider", element: <SliderPage /> },
+      //   { path: LOCATIONS.MY_PROFILE, name: "My Profile", element: <Profile /> },
+    ],
+  },
+];
