aed37d95048ac0cd1c24f3af9c9a73e36023471e
Cáp Kim Trầm <48953964+cktpro@users.noreply.github.com>
2023-10-22 17:11:16 +0700

    Revert "update wish list"

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { routers } from "../router/router";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
+import { Routes, Route } from "react-router-dom";
+import { routers } from "router/router";
 
-import productList from "../fakeData/wishlist.json";
-import Wishlist from "../components/wishlist";
+
+
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-  setProducts(productList)
-  }, []);
-
-
-  
   const renderRoutes = useCallback((routers) => {
     return routers.map((route, index) => {
       if (route.children && route.children.length > 0) {
@@ -31,13 +23,7 @@ function App() {
     });
   }, []);
 
-  return (
-    <>
-    <Router>
-      <Routes>{renderRoutes(routers)}</Routes>
-    </Router>
-    </>
-  );
+  return <Routes>{renderRoutes(routers)}</Routes>;
 }
 
 export default App;
